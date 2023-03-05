0dc498d0d4a1df0c0cbed8943ee2a7ae3d0116ec
sharangaba <e.sharangab@alustudent.com>
2023-03-05 18:55:57 +0000

    api

--- api_advanced/1-main.py
+++ api_advanced/1-main.py
@@ -1,17 +1,16 @@
 #!/usr/bin/python3
 """ 1-top_ten.py """
-import praw
+import requests
+
 
 def top_ten(subreddit):
-    reddit = praw.Reddit(
-        client_id="your_client_id",
-        client_secret="your_client_secret",
-        user_agent="your_user_agent",
-    )
-
-    try:
-        subreddit = reddit.subreddit(subreddit)
-        for submission in subreddit.hot(limit=10):
-            print(submission.title)
-    except:
+    """ prints the titles of the first 10 hot posts listed in a subreddit """
+    url = 'https://www.reddit.com/r/{}/hot.json?limit=10'.format(subreddit)
+    headers = {'User-Agent': 'Mozilla/5.0'}
+    response = requests.get(url, headers=headers, allow_redirects=False)
+    if response.status_code != 200:
         print(None)
+        return
+    posts = response.json()['data']['children']
+    for post in posts:
+        print(post['data']['title'])
