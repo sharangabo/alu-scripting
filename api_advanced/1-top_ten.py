7706b5fab6b1477e10438e104d31c1178607bd17
sharangaba <e.sharangab@alustudent.com>
2023-03-06 08:54:26 +0000

    commmit

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -1,15 +1,16 @@
 #!/usr/bin/python3
 """ 1-top_ten.py """
-import praw
+import requests
+
 
 def top_ten(subreddit):
     """ prints the titles of the first 10 hot posts listed in a subreddit """
-    try:
-        reddit = praw.Reddit(client_id='your_client_id',
-                             client_secret='your_client_secret',
-                             user_agent='your_user_agent')
-        hot_posts = reddit.subreddit(subreddit).hot(limit=10)
-        for post in hot_posts:
-            print(post.title)
-    except:
+    url = 'https://www.reddit.com/r/{}/hot.json?limit=10'.format(subreddit)
+    headers = {'User-Agent': 'Mozilla/5.0'}
+    response = requests.get(url, headers=headers, allow_redirects=False)
+    if response.status_code != 200:
         print(None)
+        return
+    posts = response.json()['data']['children']
+    for post in posts:
+        print(post['data']['title'])
